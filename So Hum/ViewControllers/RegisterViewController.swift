93ab52b1119e908144f68726752ad13b4641509f
Bingo's Code <sabareesh.kappagantu@gmail.com>
2018-02-10 17:29:44 -0800

    Added a Picker for selecting group on the Setup Page

--- So Hum/ViewControllers/RegisterViewController.swift
+++ So Hum/ViewControllers/RegisterViewController.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import FirebaseAuth
 
 class RegisterViewController: UIViewController {
     
@@ -50,6 +51,32 @@ class RegisterViewController: UIViewController {
     
     //Setting the status bar color
     override var preferredStatusBarStyle: UIStatusBarStyle {return .lightContent}
+    
+    @IBAction func onSignUp(_ sender: Any) {
+        if emailField.text == "" || nameField.text == "" || passwordField.text == "" {
+            let alertController = UIAlertController(title: "Error", message: "Please Enter your Email, Password and Name.", preferredStyle: .alert)
+            let defaultAction = UIAlertAction(title: "OK", style: .cancel, handler: nil)
+            alertController.addAction(defaultAction)
+            present(alertController, animated: true, completion: nil)
+        } else {
+            //Calling Firebase API to SignUp the User
+            Auth.auth().createUser(withEmail: emailField.text!, password: passwordField.text!, completion: { (user, error) in
+                
+                if error == nil {
+                    print("The user has successfully signed up.")
+                    
+                    //Redirect User to Setup Page
+                    
+                } else {
+                    let alertController = UIAlertController(title: "Error", message: error?.localizedDescription, preferredStyle: .alert)
+                    let defaultAction = UIAlertAction(title: "OK", style: .cancel, handler: nil)
+                    alertController.addAction(defaultAction)
+                    self.present(alertController, animated: true, completion: nil)
+                }
+                
+            })
+        }
+    }
     
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
